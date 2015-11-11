c47d811599c3dfe5a421f524691a55fe0ed3f5eb
sato1043 <sato1043@gmail.com>
2015-11-11 16:17:11 +0900

    add) async/await exaample

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
  */
 import $c from './config.js';
 import $l from './logger.js';
+import 'babel-core';
+import 'babel-polyfill';
 
 $l.info('started');
 
@@ -14,7 +16,24 @@ class AAA {
   }
 }
 
-
 const a = new AAA;
+
+
+
+// thanks to http://qiita.com/hashedhyphen/items/44701fcc18bd40066aa7
+let wait = (n) => {
+  return new Promise((done) => setTimeout(() => done(n), n));
+};
+
+let main = async () => {
+  await wait(1000);
+  console.log('await done');
+};
+
+wait(2000).then((n) => console.log(`promise normaly done: ${n}`));
+
+main();
+
+
 
 //__END__
